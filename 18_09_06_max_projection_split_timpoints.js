297d15c92b6b4d095a80acc8e4cab221d1ae8bf3
Etienne <e.schmelzer@unibas.ch>
2018-09-06 18:16:50 +0200

    generated functions, and discovered a bug in the split view function, there is a bug -> once it is called, it creates three red images, although changed channel settings are displayed, if you copy the three pictures all three pictures show only the second channel, new function required

--- 18_09_06_max_projection_split_timpoints.js
+++ 18_09_06_max_projection_split_timpoints.js
@@ -22,9 +22,10 @@ function add_channelnames(ImageID) {
 
 // this function generates a split view of the choosen image
 function splitview(ImageID) {
-  j = figureModel.panels.models[ImageID].toJSON();
+  var p = figureModel.panels.models[ImageID];
+  var j = p.toJSON();
   for (var c=0; c<j.channels.length; c++){
-      // offset to the right each time we create a new panel
+      // offset below each time we create a new panel
       j.y = j.y + (j.height * 1.05);
       // turn all channels off except for the current index
       j.channels.forEach((ch, i) => {
@@ -35,11 +36,26 @@ function splitview(ImageID) {
   };
 }
 
+// working on the next function, there is a bug in the function above, need to write a new one
+
+figureModel.panels.models[0].attributes.channels[0].active = false
+
+function splitview2(ImageID) {
+  var p = figureModel.panels.models[ImageID].toJSON();
+  var j = p.
+
+  
+  var j = figureModel.panels.models[0].attributes.channels[0].active = true;
+  j.y = j.y + (j.height * 1.05);
+  figureModel.panels.create(j);
+
+}
+
 // this function displays the time of the choosen image
 function displaytime(ImageID, displaytimemode = "hrs:mins:secs") {
-  j = figureModel.panels.models[ImageID];
+  var j = figureModel.panels.models[ImageID];
   var labels = [{
-    'time' : displaytime,
+    'time' : displaytimemode,
     'size' : 12,
     'position' : "topright",
     'color' : "ffffff"
@@ -49,13 +65,13 @@ function displaytime(ImageID, displaytimemode = "hrs:mins:secs") {
 
 // this function generates a copy of the first time point, then generates multiple
 // images of consecutive timepoints
-function multitimepoint(ImageID, columnCount, timepoints = 5, tIncrement = 1) {
+function multitimepoint(ImageID, timepoints = 5, tIncrement = 1) {
   j = figureModel.panels.models[ImageID].toJSON();
   var left = j.x;
   var top = j.y;
-  var columnCount = timepoints + 1;
+  var columnCount = timepoints;
   panelCount = 1;
-  for (var t=0; t<timepoints; t+=tIncrement){
+  for (var t=1; t<timepoints; t+=tIncrement){
       // offset to the right each time we create a new panel
       j.x = left + ((panelCount % columnCount) * j.width * 1.05);
       j.y = top + (parseInt(panelCount / columnCount) * j.height * 1.05);
@@ -76,3 +92,18 @@ function maxprojection(ImageID) {
 
 // What i want to do: have a maximum projection of a split view, and then
 // single slides of 5 consecutive timepoints
+var timepoints = 5;
+
+
+
+
+multitimepoint(0, timepoints);
+for (var p = 0; p<(timepoints); p++) {
+  splitview(p);
+};
+for (var p = 0; p<(timepoints); p++) {
+  displaytime(p);
+};
+add_channelnames(0);
+add_channelnames(5);
+add_channelnames(6);
